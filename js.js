91728457e271741bad846d611f1e7d756cb556ac
harshchahande <harshchahande123@gmail.com>
2024-03-07 18:25:45 +0530

    js.js

---
+++ js.js
@@ -0,0 +1,15 @@
+var a = document.getElementById("s1s");
+var b = document.getElementById("s4img");
+var c = document.getElementById("s2-d");
+a.style.backgroundColor = "pink";
+c.style.display = "none";
+b.style.display = "none";
+function s1() {
+  a.style.backgroundColor = "white";
+}
+function s2() {
+  c.style.display = "block";
+}
+function s4() {
+  b.style.display = "block";
+}
